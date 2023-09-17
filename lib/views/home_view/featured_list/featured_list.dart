4743351e7f068f3408ab62474cb7a97d556a3d5e
shammaskhann <shammas.csgo0312@gmail.com>
2023-09-17 21:31:31 +0500

    17/9/23 no changes occured

--- lib/views/home_view/featured_list/featured_list.dart
+++ lib/views/home_view/featured_list/featured_list.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedList extends StatelessWidget {
@@ -6,7 +7,8 @@ class FeaturedList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final databaseRef = FirebaseDatabase.instance.ref('Post');
+    final databaseRef = FirebaseStorage.instance;
+    databaseRef.ref().child('images').listAll().then((value) => print(value));
     return GridView.builder(
         padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
         itemCount: 10,
@@ -20,5 +22,30 @@ class FeaturedList extends StatelessWidget {
             color: Colors.red,
           );
         });
+    // return StreamBuilder(builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+    //   if (snapshot.hasData) {
+    //     final List list = [];
+    //     final Map<dynamic, dynamic> values = snapshot.data.value;
+    //     values.forEach((key, value) {
+    //       list.add(value);
+    //     });
+    //     return GridView.builder(
+    //         padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+    //         itemCount: list.length,
+    //         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //             crossAxisCount: 2,
+    //             crossAxisSpacing: 10,
+    //             mainAxisSpacing: 10,
+    //             childAspectRatio: 0.8),
+    //         itemBuilder: (context, index) {
+    //           return Container(
+    //             color: Colors.red,
+    //           );
+    //         });
+    //   }
+    //   return const Center(
+    //     child: CircularProgressIndicator(),
+    //   );
+    // });
   }
 }
