24ba9ce5b3c42904f475a3e5605d9f8e90ec1023
shammaskhann <shammas.csgo0312@gmail.com>
2023-09-20 23:44:03 +0500

    20/9/23 firebase connected with home screen list

---
+++ lib/view_models/controller/Featured-Items/featureitem_controller.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+class FeaturedItemController {
+  DocumentReference inventory =
+      FirebaseFirestore.instance.collection('Inventory').doc('Featured-Items');
+  Future getFeaturedItems() async {
+    try {
+      final DocumentSnapshot snapshot = await inventory.get();
+      final List list = [];
+      final Map<dynamic, dynamic> values = snapshot.data() as Map;
+      values.forEach((key, value) {
+        list.add(value);
+        print(value);
+      });
+      print(list);
+      return list;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
--- lib/views/home_view/featured_list/featured_list.dart
+++ lib/views/home_view/featured_list/featured_list.dart
@@ -1,29 +1,52 @@
+import 'package:big_cart_app/view_models/controller/Featured-Items/featureitem_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FeaturedList extends StatelessWidget {
   const FeaturedList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // final databaseRef = FirebaseStorage.instance;
-    // databaseRef.ref().child('images').listAll().then((value) => print(value));
-    DocumentReference inventory = FirebaseFirestore.instance
-        .collection('Inventory')
-        .doc('Featured-Items');
-    return GridView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-        itemCount: 10,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 0.8),
-        itemBuilder: (context, index) {
-          return Container(
-            color: Colors.red,
+    FeaturedItemController featuredItemController =
+        Get.put(FeaturedItemController());
+    return FutureBuilder(
+        future: featuredItemController.getFeaturedItems(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            return GridView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                itemCount: snapshot.data.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.8),
+                itemBuilder: (context, index) {
+                  final currentItem = snapshot.data[index];
+                  return Container(
+                    color: Colors.red,
+                    child: Text(currentItem['productName']),
+                  );
+                });
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        });
+        }));
+    // return GridView.builder(
+    //     padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+    //     itemCount: 10,
+    //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //         crossAxisCount: 2,
+    //         crossAxisSpacing: 10,
+    //         mainAxisSpacing: 10,
+    //         childAspectRatio: 0.8),
+    //     itemBuilder: (context, index) {
+    //       return Container(
+    //         color: Colors.red,
+    //       );
+    //     });
     // return StreamBuilder(builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
     //   if (snapshot.hasData) {
     //     final List list = [];
